e495982d864f872633dcbf9fb28c5dc6da66f16e
PaxtonPep <peperzakpaxton06@gmail.com>
2026-07-12 10:28:40 -0700

    Update and rename js/test.js to root/js/test.js

--- js/test.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("jsTest").textContent = "JS LOADED";
---
+++ root/js/test.js
@@ -0,0 +1,4 @@
+const jsBox = document.getElementById("jsTest");
+jsBox.textContent = "JS LOADED";
+jsBox.style.color = "red";
+jsBox.style.fontWeight = "bold";
