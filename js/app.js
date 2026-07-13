0d1fd32589f963c990b5d60b91c788340536a4c4
PaxtonPep <peperzakpaxton06@gmail.com>
2026-07-13 12:51:37 -0700

    Add function to get selected gamepasses

--- js/app.js
+++ js/app.js
@@ -5,7 +5,17 @@ import { setupUI } from "./ui.js";
 
 setupUI();
 
+function getSelectedGamepasses() {
+    const boxes = document.querySelectorAll(".gamepass");
+    const selected = [];
+    boxes.forEach(box => {
+        if (box.checked) selected.push(box.value);
+    });
+    return selected;
+}
+
 document.getElementById("generateBtn").addEventListener("click", () => {
-    const result = generateFlight(aircraft, airports);
+    const passes = getSelectedGamepasses();
+    const result = generateFlight(aircraft, airports, passes);
     document.getElementById("result").innerHTML = result;
 });
