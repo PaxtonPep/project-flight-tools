15bfb88b4e76d81bee2a46470775c09bb49c83e0
PaxtonPep <peperzakpaxton06@gmail.com>
2026-07-13 12:33:56 -0700

    Create ui.js

---
+++ js/ui.js
@@ -0,0 +1,21 @@
+export function setupUI() {
+    const modeRadios = document.querySelectorAll("input[name='mode']");
+    const projectFlightSection = document.getElementById("projectFlightSection");
+    const ptfsSection = document.getElementById("ptfsSection");
+
+    // Default: PTFS visible
+    projectFlightSection.classList.add("hidden");
+    ptfsSection.classList.remove("hidden");
+
+    modeRadios.forEach(radio => {
+        radio.addEventListener("change", () => {
+            if (radio.value === "ptfs") {
+                ptfsSection.classList.remove("hidden");
+                projectFlightSection.classList.add("hidden");
+            } else {
+                ptfsSection.classList.add("hidden");
+                projectFlightSection.classList.remove("hidden");
+            }
+        });
+    });
+}
